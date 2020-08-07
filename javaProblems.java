8b674b7b6b5147ae080a30dd8e3b4b6a9ce91530
Dksoni10 <30496460+Dksoni10@users.noreply.github.com>
2020-08-07 17:51:05 +0530

    Day 20 Assignment

---
+++ javaProblems.java
@@ -0,0 +1,36 @@
+public class Prob_Day20 {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int WORKING_DAYS = 20;
+	public static final int MAX_HOURS = 100;
+	
+	public static void main(String[] args) 
+	{
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs<=MAX_HOURS && totalWorkingDays<WORKING_DAYS) 
+		{
+			totalWorkingDays++;
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			switch ((int) empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Employee Wage is = "+ empWage);
+		}
+		System.out.println("Total Employee Wage = " + totalEmpWage);
+	}
+
+}
